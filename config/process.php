bfb9ab06f385a7faccc9b3d9c2cea0c7273dfc2f
nanju2007 <qqtd666@gmail.com>
2026-03-23 11:20:46 +0800

    Update process.php
    更新端口

--- config/process.php
+++ config/process.php
@@ -21,7 +21,7 @@ global $argv;
 return [
     'webman' => [
         'handler' => Http::class,
-        'listen' => 'http://0.0.0.0:8287',
+        'listen' => 'http://0.0.0.0:8787',
         'count' => cpu_count() * 4,
         'user' => '',
         'group' => '',
